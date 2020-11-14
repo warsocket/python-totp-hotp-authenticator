cd74ebb55074fbeb5d01aab14dd6613f33e3bf21
Bram <warsocket@gmail.com>
2020-11-14 14:01:45 +0100

    Added the actual code

---
+++ authenticator.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+import base64
+import struct
+import hmac
+from hashlib import sha1
+from time import time
+
+def safeb32decode(data):
+	if ((len(data) * 5 ) % 8) >= 5: data = data[:-1] #last character encodes no new byte
+	padcount = (8-(len(data)%8))%8 #calculate padding
+	data += "="*padcount
+	return(base64.b32decode(data, True))
+
+
+def hotp(secret, generation):
+	digest = hmac.new(
+		safeb32decode(secret),
+		struct.pack("!Q", generation),
+		sha1
+		).digest()
+
+	index = digest[19] & 0xF
+	number = (struct.unpack("!I", digest[index:index+4])[0] & 0x7FFFFFFF) % 10**6
+	return "{:0>6}".format(number)
+
+
+def totp(secret):
+	return hotp(secret, int(time()) // 30)
+
+
+# print(totp("SECRETSECRETSECRET")) #this will print the google auth timed code for the secret "SECRETSECRETSECRET"
